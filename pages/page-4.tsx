1c53e1defe358e83dac7359f408f8741fda6be29
Chukwudobe Micah <micah@refine.agency>
2024-09-30 17:59:00 +0100

    chore: changes

--- pages/page-4.tsx
+++ pages/page-4.tsx
@@ -42,11 +42,14 @@ export default function Home() {
       <div className="grid bg-[#171A20] px-4 py-6  grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {cardData.map(({ title, buttonText }, index) => {
           return (
-            <div key={index} className="border-b border-b-[#818AB1] pb-4">
-              <div className="relative cursor-pointer group overflow-hidden bg-background h-[150px] rounded-lg shadow-lg p-4 flex items-center gap-4 w-full">
-                <div className="flex flex-col justify-between h-full">
+            <div key={index} className="border-b group border-b-[#818AB1] pb-4">
+              <div className="relative group cursor-pointer overflow-hidden bg-background h-[150px] rounded-lg shadow-lg p-4 flex items-center gap-4 w-full">
+                <div className="flex flex-col  justify-between h-full">
                   <h3 className="text-white text-xl font-bold">{title}</h3>
-                  <Button variant="outline" className="mt-2 w-fit text-sm ">
+                  <Button
+                    variant="outline"
+                    className="group-hover:!bg-primary mt-2 w-fit text-sm "
+                  >
                     {buttonText}
                   </Button>
                 </div>
